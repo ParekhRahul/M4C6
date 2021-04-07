ed78aa754e350427bf791248f5665c7ddd477eca
rahul <iamrahulparekh@gmail.com>
2021-04-06 19:23:03 -0600

    Initial Commit

---
+++ M4C6/M4C6App.swift
@@ -0,0 +1,17 @@
+//
+//  M4C6App.swift
+//  M4C6
+//
+//  Created by Rahul Parekh on 2021-04-06.
+//
+
+import SwiftUI
+
+@main
+struct M4C6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
